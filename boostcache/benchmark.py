7609cf0942e8ed308a368b1eb54393e0fa8d1093
Qolt <ysemenov.mail@gmail.com>
2013-12-31 20:41:49 +0400

    Added new options to benchmark script.

--- boostcache/benchmark.py
+++ boostcache/benchmark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys, select, time, os
+import sys, select, time, os, re
 import datetime
 import optparse
 import string
@@ -10,19 +10,31 @@ import redis
 
 KB = 1024
 
-def init_parser(options):                                                     
-           options.add_option("-n", "--value_number",                         
-                              action="store",                                
-                              dest="value_number",                 
-                              help="Number of values will be generated to benchmark boostcache for one client.")
-           options.add_option("-c", "--clients_number",                         
-                              action="store",                                
-                              dest="clients_number",                 
-                              help="Number of parallel clients.")
+def init_parser(options):
+           options.add_option("-n", "--value_number",
+                              action = "store",
+                              default = 200,
+                              dest = "value_number",
+                              help = "Number of values will be generated to benchmark boostcache for one client. Default is 200.")
+           options.add_option("-c", "--clients_number",
+                              action = "store",
+                              default = 50,
+                              dest = "clients_number",
+                              help = "Number of parallel clients. Default is 50.")
            options.add_option("-s", "--server_socket",
-                              action="store",                                
-                              dest="server_socket",                 
-                              help="Path to the server socket.")
+                              action = "store",
+                              dest = "server_socket",
+                              help = "Path to the server socket.")
+           options.add_option("-a", "--address",
+                              action = "store",
+                              default = "localhost:9876",
+                              dest = "address",
+                              help = "Boostcache server address. Default is localhost:9876.")
+           options.add_option("-l", "--keys_length",
+                              action = "store",
+                              default = "1K",
+                              dest = "length",
+                              help = "Keys length. 1M = 1024K = 1048576 symbols. E. g. -l 5K, for keys length 5120 symbols. Default is 1K.")
 
 def check_options(options):
         try:
@@ -35,14 +47,29 @@ def check_options(options):
             if not os.path.exists(options.server_socket):
                 print "Could not find server socket", options.server_socket
                 raise SystemExit(1)
+        else:
+            if not re.match("^([a-zA-Z]+|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})):[0-9]+$", options.address):
+                print "Invalid server address:", options.address
+                raise SystemExit(1)
+        if re.match("[0-9]+[mMkK]{0,1}$", options.length):
+            if options.length[::-1][0] == 'm' or options.length[::-1][0] == 'M':
+                options.length = int(options.length[:-1]) * KB * KB
+            elif options.length[::-1][0] == 'k' or options.length[::-1][0] == 'K':
+                options.length = int(options.length[:-1]) * KB
+            else:
+                options.length = int(options.length)
+        else:
+            print "Invalid keys length:", options.length
+            raise SystemExit(1)
+
 
 def rand_word(length):
     return ''.join(random.choice(string.letters) for x in range(length))
 
-def create_dict(number):
+def create_dict(number, length):
     keys_dict = {}
     for i in range(number):
-        keys_dict.update({rand_word(KB) : rand_word(KB)}) 
+        keys_dict.update({rand_word(length) : rand_word(length)})
     return keys_dict
 
 
@@ -50,10 +77,12 @@ class boostcache_benchmark():
     def __init__(self, number, clients_number, keys_dict, socket, host = 'localhost', port = 9876):
         self.keys_number = number
         self.clients_number = clients_number
-        self.keys_dict = keys_dict 
+        self.keys_dict = keys_dict
         self.test_number = number
         self.clients_dict = {}
         self.socket = socket
+        self.host = host
+        self.port = port
         self.epoll = select.epoll()
         self.create_connections()
 
@@ -66,9 +95,9 @@ class boostcache_benchmark():
             client.ping()
             self.clients_dict[client.connection._sock.fileno()] = client
             self.epoll.register(client.connection._sock.fileno(), select.EPOLLOUT)
-    
+
     def run(self, function):
-        one_percent = (self.keys_number * self.clients_number) / 100
+        one_percent = (self.keys_number * self.clients_number) / 100.0
         total_time = 0.0
         tests = 0
         for key, value in self.keys_dict.items():
@@ -79,7 +108,8 @@ class boostcache_benchmark():
                 if tests % (one_percent * 10) == 0:
                     print "Done:", (tests / one_percent), "% -- ", total_time, "seconds"
         print "Total: ", total_time, "seconds. Number of tests:", tests
-        print "%8.3f requests per second" % (tests / total_time)
+        if total_time:
+            print "%8.3f requests per second" % (tests / total_time)
 
     def hset_benchmark(self, client, key, value):
         start_time = time.clock()
@@ -90,22 +120,22 @@ class boostcache_benchmark():
         start_time = time.clock()
         client.hget(key)
         return time.clock() - start_time
-            
+
     def hdel_benchmark(self, client, key, value):
         start_time = time.clock()
         client.hdel(key)
         return time.clock() - start_time
-        
+
     def atset_benchmark(self, client, key, value):
         start_time = time.clock()
         client.atset(key, value)
         return time.clock() - start_time
-            
+
     def atget_benchmark(self, client, key, value):
         start_time = time.clock()
         client.atget(key)
         return time.clock() - start_time
-            
+
     def atdel_benchmark(self, client, key, value):
         start_time = time.clock()
         client.atdel(key)
@@ -124,15 +154,17 @@ class boostcache_benchmark():
         self.run(self.atget_benchmark)
         print "ATDEL BENCHMARK:"
         self.run(self.atdel_benchmark)
-                    
+
 if __name__ == "__main__":
     parser = optparse.OptionParser()
     init_parser(parser)
     (options, args) = parser.parse_args()
     check_options(options)
-    keys_dict = create_dict(options.value_number)
-    benchmark = boostcache_benchmark(options.value_number, 
-                                     options.clients_number, 
-                                     keys_dict, 
-                                     options.server_socket)
+    keys_dict = create_dict(options.value_number, options.length)
+    benchmark = boostcache_benchmark(options.value_number,
+                                     options.clients_number,
+                                     keys_dict,
+                                     options.server_socket,
+                                     host = options.address.split(':')[0],
+                                     port = options.address.split(':')[1])
     benchmark.run_hbenchmark()
